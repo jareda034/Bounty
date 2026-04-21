3da24161f3236908a61b99cdfd2dcf96feb08d9b
jareda034 <jakigbesote1@gmail.com>
2026-04-21 16:06:04 +0100

    add logic to PlayerMovementController.cs for isometric movement

--- Bounty/Assets/Scripts/PlayerMovementController.cs
+++ Bounty/Assets/Scripts/PlayerMovementController.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
    // Variables
    [SerializeField] float playerSpeed = 5.0f;
+   [SerializeField] float rotationSpeed = 360f;
    Rigidbody rb;
-   Vector2 playerMovement;
+   Vector3 playerMovement;
 
    void Awake()
     {
@@ -16,6 +18,7 @@ public class PlayerMovementController : MonoBehaviour
     void FixedUpdate()
     {
         MovePlayer();
+        Look();
     }
 
     void OnMove(InputValue value)
@@ -27,5 +30,15 @@ public class PlayerMovementController : MonoBehaviour
     {
         Vector3 movement = new Vector3(playerMovement.x * playerSpeed, rb.linearVelocity.y, playerMovement.y * playerSpeed);
         rb.linearVelocity = movement;
+    }
+
+    void Look()
+    {
+        if (playerMovement == Vector3.zero) return;
+        Matrix4x4 isometricMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45 , 0));
+        Vector3 multipLyMatrix = isometricMatrix.MultiplyPoint3x4(playerMovement);
+
+        Quaternion rotation = Quaternion.LookRotation(multipLyMatrix, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
 }
