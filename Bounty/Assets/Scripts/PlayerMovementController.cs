b78855ef2e4f965c7054aa2e82e17b819a6c109e
jareda034 <jakigbesote1@gmail.com>
2026-04-27 13:00:13 +0100

    Add player rotation on mouse position

--- Bounty/Assets/Scripts/PlayerMovementController.cs
+++ Bounty/Assets/Scripts/PlayerMovementController.cs
@@ -8,7 +8,9 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] float playerSpeed = 5.0f;
     [SerializeField] float rotationSpeed = 0.15f;
     Rigidbody rb;
-    Vector3 playerMovement;
+    Vector3 playerMovement, rotationTarget;
+    Vector2 mouseLook;
+
 
     void Awake()
     {
@@ -17,6 +19,8 @@ public class PlayerMovementController : MonoBehaviour
 
     void FixedUpdate()
     {
+        MousePosition();
+        RotatePlayer();
         MovePlayer();
     }
 
@@ -25,17 +29,36 @@ public class PlayerMovementController : MonoBehaviour
         playerMovement = value.Get<Vector2>();
     }
 
+    void OnLook(InputValue value)
+    {
+        mouseLook = value.Get<Vector2>();
+    }
+
     void MovePlayer()
     {
         Vector3 movement = new Vector3(playerMovement.x * playerSpeed, rb.linearVelocity.y, playerMovement.y * playerSpeed);
-
-        if (movement != Vector3.zero)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotationSpeed);
-        }
-
-
-
         rb.linearVelocity = movement;
+    }
+
+    void MousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(mouseLook);
+        if (Physics.Raycast(ray , out hit))
+        {
+            rotationTarget = hit.point;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        var lookPos = rotationTarget - transform.position;
+        lookPos.y = 0f;
+        var rotation = Quaternion.LookRotation(lookPos);
+        Vector3 aimDirection = new Vector3(rotationTarget.x, 0f, rotationTarget.z);
+        if (aimDirection != Vector3.zero)
+        {
+             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed);
+        }
     }
 }
