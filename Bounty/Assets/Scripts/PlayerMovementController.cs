8432c137fdadc406f45b46bcd455f4a2cf67b173
jareda034 <jakigbesote1@gmail.com>
2026-04-27 12:33:26 +0100

    Update PlayerMovementController.cs to fix player snapping

--- Bounty/Assets/Scripts/PlayerMovementController.cs
+++ Bounty/Assets/Scripts/PlayerMovementController.cs
@@ -4,13 +4,13 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
-   // Variables
-   [SerializeField] float playerSpeed = 5.0f;
-   [SerializeField] float rotationSpeed = 360f;
-   Rigidbody rb;
-   Vector3 playerMovement;
+    // Variables
+    [SerializeField] float playerSpeed = 5.0f;
+    [SerializeField] float rotationSpeed = 0.15f;
+    Rigidbody rb;
+    Vector3 playerMovement;
 
-   void Awake()
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
@@ -28,7 +28,12 @@ public class PlayerMovementController : MonoBehaviour
     void MovePlayer()
     {
         Vector3 movement = new Vector3(playerMovement.x * playerSpeed, rb.linearVelocity.y, playerMovement.y * playerSpeed);
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotationSpeed);
+        }
+
 
 
         rb.linearVelocity = movement;
